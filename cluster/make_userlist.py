6c054efeb754ca889ccea8e37b8e0884b7b16efc
junishitsuka <ishitsuka.jun@gmail.com>
2014-10-25 16:22:55 +0900

    Fixed the program for subclustering

--- cluster/make_userlist.py
+++ cluster/make_userlist.py
@@ -1,22 +1,24 @@
 #! /usr/bin/python
 # coding: utf-8
 
-import sys
+import sys, os
 CLUSTER_MAX_NUMBER = 10000
 
 def main():
-    f = open('../../cluster/community/community_propagation_20140530/All//community_userid.csv', 'r')
-    line = f.readline()
-    line = f.readline() # ignore the header line
-    while line:
-        user = line.rstrip().split(',')
-        print int(user[1])
-        if (int(user[1]) < CLUSTER_MAX_NUMBER): break # clusterのメンバー数がしきい値より下ならループをbreak
-        fw = open('../../cluster/community/community_propagation_20140530/Sub/cluster_%s/userlist.txt' % user[0], 'w')
-        for u in user[2:]:
-            fw.write(u + '\n')
-        fw.close()
+    for i in range(26):
+        f = open('../../cluster/community/community_propagation_20140530/Sub/cluster_%d/community_userid.csv' % (i + 1), 'r')
         line = f.readline()
+        line = f.readline() # ignore the header line
+        while line:
+            user = line.rstrip().split(',')
+            print int(user[1])
+            if (int(user[1]) < CLUSTER_MAX_NUMBER): break # clusterのメンバー数がしきい値より下ならループをbreak
+            os.mkdir('../../cluster/community/community_propagation_20140530/Sub/cluster_%d_%s/' % (i + 1, user[0]))
+            fw = open('../../cluster/community/community_propagation_20140530/Sub/cluster_%d_%s/userlist.txt' % (i + 1, user[0]), 'w')
+            for u in user[2:]:
+                fw.write(u + '\n')
+            fw.close()
+            line = f.readline()
 
 if __name__ == '__main__':
     main()
