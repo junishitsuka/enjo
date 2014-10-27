963880cf5476f7862c232b4b4083f881745c7f6a
junishitsuka <ishitsuka.jun@gmail.com>
2014-10-27 10:15:47 +0900

    Modified diffusion.py to get all tweet

--- diffusion/diffusion.py
+++ diffusion/diffusion.py
@@ -4,9 +4,10 @@
 import MySQLdb
 import networkx as nx
 import matplotlib.pylab as plt
-import datetime
+import datetime, sys
 
 TWEETID = '460607471132766208'
+LIMIT = 100000
 
 class diffusion:
     def __init__(self):
@@ -17,6 +18,11 @@ class diffusion:
         sql = 'SELECT b.name, b.time, b.id, b.tweet_id, u.`spirits_communityid` FROM enjo_basedata as b left join enjo_users_fixed as u on b.name = u.name where retweet_id = %s order by b.time asc' % tweetid
         self.cursor.execute(sql)
         return {x[0]: x[1:] for x in list(self.cursor.fetchall())}
+
+    def getnode(self, tweetid, limit = 10):
+        sql = 'select name from enjo_basedata where retweet_id = "%s" order by time limit %d' % (tweetid, limit)
+        self.cursor.execute(sql)
+        return [x[0] for x in self.cursor.fetchall()]
 
     def getlink(self, name):
         link = []
@@ -38,7 +44,8 @@ class diffusion:
         retweet['jyunichidesita'] = (datetime.datetime(2014, 4, 28, 0, 0, 0),) # 炎上ツイートのユーザーのみの対応
         userlist = retweet.keys()
         # node = self.getRTchannel('jyunichidesita', userlist, retweet) # 炎上ツイートをRTし、且つそのツイートのユーザーとリンクのあるユーザーの名前のリスト
-        node = ['reichi062', 'hasiayu', '2nethi', 'NatsukiPydyhgfd', 'soujiro0725', 'Kirokuro', 'Heavymetalskier', 'maiayumio', 'azullechuza', 'hakofuguf']
+        # 炎上ツイートを始めにRetweetしたユーザーをLIMIT件取得
+        node = self.getnode(TWEETID, LIMIT)
         while node:
             next = []
             for n in node:
@@ -53,13 +60,13 @@ class diffusion:
         close = nx.closeness_centrality(G)
         bet = nx.betweenness_centrality(G)
         eigen = nx.eigenvector_centrality(G)
-        f = open('../../data/output/diffusion/centrality.csv', 'w')
+        f = open('../../data/output/diffusion/centrality_all.csv', 'w')
         for k,v in sorted(degree.items(), key=lambda x: x[1], reverse=True):
             f.write(str(userlist[k]) + ',' + str(retweet[userlist[k]][0]) + ',' + str(retweet[userlist[k]][3]) + ',' + str(v) + ',' + str(close[k]) + ',' + str(bet[k]) + ',' + str(eigen[k]) + '\n')
         f.close()
 
         nx.draw(G, node_size=50)
-        plt.savefig("../../data/output/diffusion/undirected.png")
+        plt.savefig("../../data/output/diffusion/undirected_all.png")
         plt.show()
 
 if __name__ == '__main__':
